ddc550d62dcef1e64205f7d59b1a2342e52605c7
lucca <luccalf13@gmail.com>
2026-06-29 19:49:02 -0300

    class CategoriaRepository.php

---
+++ repository/CategoriaRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class CategoriaRepository {
+
+    private PDO $pdo;
+
+    public function __construct() {
+        $this->pdo = Database::getConnection();
+    }
+
+    public function listarTodos(): array {
+        $sql = "SELECT * FROM categoria ORDER BY nome";
+        $stmt = $this->pdo->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
